cdcc30c10ce37956246b6f9b5362fa94bf40852d
Matheus Almeida <matheus.almeida@getninjas.com.br>
2023-03-09 17:35:44 -0300

    feat: Add isolation level for development of this emacs.d for not conflict with doom emacs wich is the editor I current use until my config is finished

---
+++ emacs.d/isolation/isolate.el
@@ -0,0 +1,4 @@
+
+(setq package-user-dir "~/Workspaces/Personal/dotfiles/emacs.d/packages")
+
+(setq runemacs/default-font-size 280)
---
+++ emacs.d/run-emacs.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+emacs -Q --load ~/Workspaces/Personal/dotfiles/emacs.d/isolation/isolate.el --load ~/Workspaces/Personal/dotfiles/emacs.d/init.el

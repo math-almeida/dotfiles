4f4351431d75eb3178dbfbc7e1a5150e8028f45e
Matheus Almeida <matheusalmeida025@gmail.com>
2023-04-13 02:16:39 -0300

    feat: Create keybiding for magit-blame and for flymake diagnostic buffer

--- Zsh/zsh_custom/docker.plugin.zsh
+++ Zsh/zsh_custom/docker.plugin.zsh
@@ -3,6 +3,7 @@ alias dstatus="sudo systemctl status docker"
 alias dstart="sudo systemctl start docker"
 alias dexec="docker exec -it"
 alias dattach="docker attach"
+alias dlogs="docker logs"
 
 # docker-compose alias
 alias compose="docker-compose"

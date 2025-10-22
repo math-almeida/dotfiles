b9185f26097354d445b3e415cc3e66ee942d0547
Matheus Almeida <matheusalmeida025@gmail.com>
2025-10-22 10:52:38 -0300

    Add terraform workspace aliases

--- Zsh/zsh_custom/terraform.plugin.zsh
+++ Zsh/zsh_custom/terraform.plugin.zsh
@@ -7,6 +7,12 @@ alias tfde="terraform destroy"
 alias tfco="terraform console"
 alias tfva="terraform validate"
 
+# terraform workspaces
+alias tfwon="terraform workspace new"
+alias tfwos="terraform workspace select"
+alias tfwol="terraform workspace list"
+alias tfwod="terraform workspace delete"
+
 # Commands with auto-aprove
 alias tfapy="terraform apply -auto-approve"
 alias tfdey="terraform destroy -auto-approve"
